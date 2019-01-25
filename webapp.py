4a401704fff1ae3d87cee7a42bda646996713c02
BCRXVI <42652884+BCRXVI@users.noreply.github.com>
2019-01-25 11:01:29 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,6 +1,9 @@
 from flask import Flask, url_for, render_template, request
-
+import os
 app = Flask(__name__)
+
+@app.route("/")
+def render_main():
 
 if __name__=="__main__":
     app.run(debug=True, port=66666)
