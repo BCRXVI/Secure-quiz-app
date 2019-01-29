b2f1b1f9944edca909dbf2785774fbabafba3721
BCRXVI <42652884+BCRXVI@users.noreply.github.com>
2019-01-29 10:59:35 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -2,23 +2,23 @@ from flask import Flask, url_for, render_template, request
 import os
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/page1")
 def render_main():
     return render_template('layout.html')
 
 
-@app.route("/page1")
-def render_page1():
-    return render_template('page1.html')
-
-
 @app.route("/page2")
 def render_page2():
-    return render_template('page2.html')
+    return render_template('page1.html')
 
 
 @app.route("/page3")
 def render_page3():
+    return render_template('page2.html')
+
+
+@app.route("/page4")
+def render_page4():
     return render_template('page3.html')
 
 if __name__=="__main__":
