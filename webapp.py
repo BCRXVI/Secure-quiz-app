cee7e99714942a068e4c111a270fcff9f8f9c1fe
BCRXVI <42652884+BCRXVI@users.noreply.github.com>
2019-01-30 10:46:57 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -2,6 +2,11 @@ from flask import Flask, url_for, render_template, request
 import os
 app = Flask(__name__)
 
+@app.route("/")
+def render_main():
+    return render_template('layout.html')
+
+
 @app.route("/page1")
 def render_main():
     return render_template('page1.html')
