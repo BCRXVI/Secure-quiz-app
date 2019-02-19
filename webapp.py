faba2222e2930e015a67369a4dd4eda5ad92346a
BCRXVI <42652884+BCRXVI@users.noreply.github.com>
2019-02-19 12:42:45 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -4,27 +4,17 @@ app = Flask(__name__)
 
 @app.route("/")
 def render_main():
-    return render_template('layout.html')
+    return render_template('page1.html')
 
 
 @app.route("/page1")
 def render_main():
-    return render_template('page1.html')
+    return render_template('page2.html')
 
 
 @app.route("/page2")
 def render_page2():
-    return render_template('page2.html')
-
-
-@app.route("/page3")
-def render_page3():
     return render_template('page3.html')
-
-
-@app.route("/page4")
-def render_page4():
-    return render_template('page4.html')
 
 if __name__=="__main__":
     app.run(debug=True, port=66666)
