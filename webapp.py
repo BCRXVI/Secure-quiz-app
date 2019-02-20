cc12c4ba991cb75fc525104c803abccb0cab7895
BCRXVI <42652884+BCRXVI@users.noreply.github.com>
2019-02-20 12:45:49 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -4,7 +4,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def render_main():
-    if(buildings="Burj Khalifa"
+    rQ = 0
+    if(buildings="Burj   Khalifa")
+        rQ + 1 = rQ
+    
     return render_template('page1.html')
 
 
@@ -20,6 +23,10 @@ def render_page2():
 @app.route("/page3")
 def render_page3():
     return render_template('page4.html')
+       
+@app.route("/page4")
+def render_page4():
+       return rQ
 
 if __name__=="__main__":
     app.run(debug=True, port=66666)
