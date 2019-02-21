74bdecc0ae9ec1c913e059aee5b3e77169c6ff1b
BCRXVI <42652884+BCRXVI@users.noreply.github.com>
2019-02-21 10:38:07 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -4,29 +4,36 @@ app = Flask(__name__)
 
 @app.route("/")
 def render_main():
-    rQ = 0
-    if(buildings="Burj   Khalifa")
-        rQ + 1 = rQ
-    
     return render_template('page1.html')
 
 
 @app.route("/page1")
 def render_page1():
+    if(request.form["buildings"] == "burj khalifa"):
+        session["question1"] = "correct"
+    else:
+        session["question1"] = "incorrect"
     return render_template('page2.html')
-
 
 @app.route("/page2")
 def render_page2():
+    if(request.form["numbers"] == "33"):
+        session["question2"] = "correct"
+    else:
+        session["question2"] = "incorrect"
     return render_template('page3.html')
 
 @app.route("/page3")
 def render_page3():
+    if(request.form["capitals"] == "washington d.c."):
+        session["question3"] = "correct"
+    else:
+        session["question3"] = "incorrect"
     return render_template('page4.html')
        
 @app.route("/page4")
 def render_page4():
-       return rQ
+       return 
 
 if __name__=="__main__":
     app.run(debug=True, port=66666)
